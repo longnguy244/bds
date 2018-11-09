3cdd8c2900fe387aa19ce4e38e95a2627cb6e3b1
nhinyn <nhinyn143@gmail.com>
2018-11-09 14:33:45 +0700

    fgf

--- resources/views/admin/pages/hopdong/show.blade.php
+++ resources/views/admin/pages/hopdong/show.blade.php
@@ -1,35 +1,5 @@
-<<<<<<< HEAD
 
-<html>
 
-	<head>
-		<meta charset="UTF-8">
-		<title>Nội dung hợp đồng</title>
-		<style>
-		 
-            .header{
-                text-align: center;
-                line-height: 0.6;
-            }
-			
-            .dream{
-                margin-left: 1.5 px;
-            }
-		</style>
-	</head>	
-		<body>
-        <div class="header">
-        <h4>CỘNG HÒA XÃ HỘI CHỦ NGHĨA VIỆT NAM</h4>
-        <p>Độc lập - Tự do - Hạnh phúc</p>
-        <p>----------</p>
-        <h4>HỢP ĐỒNG MÔI GIỚI BẤT ĐỘNG SẢN<h4>
-        <p>(Số:{{$data->id}}/HDMGBDS)
-        </div>
-        
-        <div class="dream">
-        Hôm nay ngày{{$data->ngaylaphd}} tháng năm</br></br>
-        <b>Chúng tôi tên:{{$data->ten_kh}}</b>
-=======
 <!DOCTYPE html>
 <html lang="vn">
 <head>
@@ -51,13 +21,14 @@
             <p>Độc lập - Tự do - Hạnh phúc</p>
             <p>----------</p>
             <h4>HỢP ĐỒNG MÔI GIỚI BẤT ĐỘNG SẢN</h4>
-            <p>(Số:....../HDMGBDS)</p>
+            <p>(Số:{{$data->id}}/HDMGBDS)
+
         </div>
 
         <div class="">
             <p>Hôm nay ngày tháng năm</p>
             <p><b>Chúng tôi tên:</b></p>
->>>>>>> 7b2cc49a1f87a839c8c20842482a6225d9dcdef0
+
         </div>
         <a href="{{ route('down.hopdong',$data->id) }}">Xuat File</a>
     </center>   
